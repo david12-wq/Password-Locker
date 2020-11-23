93f4b4f0e6d16761f2bbaccd3c5d2aaa42aaf930
david12-wq <davidkamau245@gmail.com>
2020-11-23 11:42:20 +0300

    Create class that generates new instances of users.

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    Class that generates new instances of users.
+    """
+
+    user_list = []
+    def __init__(self,first_name,second_name,username,password):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.username = username
+        self.password = password
+
+    
