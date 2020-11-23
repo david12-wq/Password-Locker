789324bbe94057babf713fe6e4f3f981b5b1e3c7
david12-wq <davidkamau245@gmail.com>
2020-11-23 16:39:26 +0300

    create classmethod

--- user.py
+++ user.py
@@ -10,4 +10,33 @@ class User:
         self.username = username
         self.password = password
 
-  
+    def save_user(self):
+
+        """
+        save_user method saves user objects into user_list
+        """
+
+        User.user_list.append(self) 
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+        User.user_list.remove(self)    
+
+    @classmethod
+    def find_user_by_username(cls,username):
+
+        '''
+        Method that takes in a username and returns a user that matches that username.
+        Args:
+            username: username to search for
+        Returns :
+            User that matches the username.
+        '''
+        for user in cls.user_list:
+            if user.username == username:
+                return user
+
+   
