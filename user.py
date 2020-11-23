f3185246eb567237272a4ab7ce378b326d0ed01d
david12-wq <davidkamau245@gmail.com>
2020-11-23 11:47:42 +0300

    Add method tha checks if a user exists.

--- user.py
+++ user.py
@@ -39,4 +39,17 @@ class User:
             if user.username == username:
                 return user
 
-    
+    @classmethod
+    def user_exist(cls,username):
+        '''
+        Method that checks if a user exists from the user list.
+        Args:
+            username: username to search if it exists
+        Returns :
+            Boolean: True or false depending if the user exists
+        '''
+        for user in cls.user_list:
+            if user.username == username:
+                    return True
+
+        return False   
