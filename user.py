669294409c54cc86695a8f1669f8df38c0123c50
david12-wq <davidkamau245@gmail.com>
2020-11-23 11:44:17 +0300

    Add method that saves user objects into a user list and also add method that deletes a user that has been saved into a user list.

--- user.py
+++ user.py
@@ -10,4 +10,19 @@ class User:
         self.username = username
         self.password = password
 
-    
+    def save_user(self):
+
+        """
+        save_user method saves user objects into user_list
+        """
+
+        User.user_list.append(self) 
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+        User.user_list.remove(self)    
+
+   
